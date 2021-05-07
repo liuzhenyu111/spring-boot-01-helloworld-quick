8c771da4a5669850131a3e014698154633db5190
Lzy <jetliuzhenyu@163.com>
2021-05-07 14:27:32 +0800

    second commit

--- src/main/java/com/atguigu/springboot/SpringBoot01HelloworldQuickApplication.java
+++ src/main/java/com/atguigu/springboot/SpringBoot01HelloworldQuickApplication.java
@@ -9,6 +9,7 @@ public class SpringBoot01HelloworldQuickApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringBoot01HelloworldQuickApplication.class, args);
         //随便写写
+        //再写写
     }
 
 }
